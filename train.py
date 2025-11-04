c82978183f9a759cb8951737d8a4d8294d9f581a
Josh Gaughan <jdgaugha@iu.edu>
2025-11-04 14:33:36 +0000

    fit_linear_regression() comments and docstring

--- train.py
+++ train.py
@@ -16,33 +16,51 @@ def get_coffee_data():
     df_coffee (pd.DataFrame): A dataframe produced
     from the data within the url
     '''
-    # Read the raw coffee csv from the url
+    # Read the raw coffee csv from the url.
     df_coffee = pd.read_csv("https://raw.githubusercontent.com/leontoddjohnson/datasets/refs/heads/main/data/coffee_analysis.csv")
 
-    # Drop duplicate instances
+    # Drop duplicate instances.
     df_coffee.drop_duplicates(subset = 'desc_1', inplace = True)
 
-    # Drop NA values
+    # Drop NA values.
     df_coffee.dropna(subset = ['desc_1', 'roast', 'loc_country'], inplace = True)
 
     return df_coffee
 
 def fit_linear_regression():
+    '''
+    Fits a linear regression with 100g_USD as the
+    lone predictor and rating as the response.
 
+    Parameters:
+    No input values
+
+    Return value:
+    No return value but it does produce a pickle file
+    containing the trained model
+    '''
+    # Establish the data set.
     df_coffee = get_coffee_data()
 
+    # Do a train, test set split using 80/20.
     df_train, df_test = train_test_split(df_coffee, test_size = 0.2, random_state = 42)
 
+    # The only feature (predictor) is defined as 100g_USD.
     features = ['100g_USD']
 
+    # Only the features in the train set.
     X = df_train[features]
 
+    # Only the response in the train set.
     y = df_train['rating']
 
+    # Initiate a LinearRegression object.
     lm = LinearRegression()
 
+    # Fit the model using the features and response.
     lm.fit(X, y)
 
+    # Create a pickle file with the model.
     try:
         with open('model_1.pickle', 'wb') as file:
             pickle.dump(lm, file)
